cbce0e88910eeeb3a3316f47c9efcf1f3216daaf
Athit9838 <35733772+Athit9838@users.noreply.github.com>
2020-11-11 16:14:04 +0700

    cv2'

--- app.py
+++ app.py
@@ -17,53 +17,53 @@ def hello_world():
 @app.route('/api')
 def api():
     return 'Hello, API!'
-    # file = request.files['image'].read()
-    # npimg = np.fromstring(file, np.uint8)
-    # img = cv2.imdecode(npimg, 1)
+    file = request.files['image'].read()
+    npimg = np.fromstring(file, np.uint8)
+    img = cv2.imdecode(npimg, 1)
 
-    # if img.shape[:2] == (3024, 4032) or img.shape[:2] == (960, 1280):
-    #     img = cv2.resize(img, (806, 605))
-    # if img.shape[:2] == (4032, 3024) or img.shape[:2] == (1280, 960):
-    #     img = cv2.resize(img, (605, 806))
+    if img.shape[:2] == (3024, 4032) or img.shape[:2] == (960, 1280):
+        img = cv2.resize(img, (806, 605))
+    if img.shape[:2] == (4032, 3024) or img.shape[:2] == (1280, 960):
+        img = cv2.resize(img, (605, 806))
 
-    # gray = cv2.cvtColor(img, cv2.COLOR_BGR2GRAY)
-    # mask1 = np.zeros(img.shape[:2], np.uint8)
+    gray = cv2.cvtColor(img, cv2.COLOR_BGR2GRAY)
+    mask1 = np.zeros(img.shape[:2], np.uint8)
 
-    # circles1 = cv2.HoughCircles(gray, cv2.HOUGH_GRADIENT, 1, 200)
+    circles1 = cv2.HoughCircles(gray, cv2.HOUGH_GRADIENT, 1, 200)
 
-    # for i in circles1[0, :]:
-    #     i[2] = i[2]+4
-    #     cv2.circle(mask1, (i[0], i[1]), i[2], (255, 255, 255), thickness=-1)
+    for i in circles1[0, :]:
+        i[2] = i[2]+4
+        cv2.circle(mask1, (i[0], i[1]), i[2], (255, 255, 255), thickness=-1)
 
-    # masked_data = cv2.bitwise_and(img, img, mask=mask1)
-    # _, thresh = cv2.threshold(mask1, 127, 255, cv2.THRESH_BINARY)
+    masked_data = cv2.bitwise_and(img, img, mask=mask1)
+    _, thresh = cv2.threshold(mask1, 127, 255, cv2.THRESH_BINARY)
 
-    # contours = cv2.findContours(
-    #     thresh, cv2.RETR_EXTERNAL, cv2.CHAIN_APPROX_SIMPLE)[0]
-    # x, y, w, h = cv2.boundingRect(contours[0])
+    contours = cv2.findContours(
+        thresh, cv2.RETR_EXTERNAL, cv2.CHAIN_APPROX_SIMPLE)[0]
+    x, y, w, h = cv2.boundingRect(contours[0])
 
-    # crop = masked_data[y:y+h, x:x+w]
-    # output = crop.copy()
+    crop = masked_data[y:y+h, x:x+w]
+    output = crop.copy()
 
-    # gray2 = cv2.cvtColor(output, cv2.COLOR_BGR2GRAY)
-    # cimg = cv2.fastNlMeansDenoising(gray2, 5, 10, 5)
+    gray2 = cv2.cvtColor(output, cv2.COLOR_BGR2GRAY)
+    cimg = cv2.fastNlMeansDenoising(gray2, 5, 10, 5)
 
-    # circles = cv2.HoughCircles(cimg, cv2.HOUGH_GRADIENT, 1, 10, param1=15,
-    #                            param2=20, minRadius=10, maxRadius=22)
-    # circles = np.uint16(np.around(circles))[0, :]
-    # count = 0
-    # for i in range(len(circles)):
-    #     count = count+1
-    #     cv2.circle(output, (circles[i][0], circles[i]
-    #                         [1]), circles[i][2], (0, 255, 0), 2)
+    circles = cv2.HoughCircles(cimg, cv2.HOUGH_GRADIENT, 1, 10, param1=15,
+                               param2=20, minRadius=10, maxRadius=22)
+    circles = np.uint16(np.around(circles))[0, :]
+    count = 0
+    for i in range(len(circles)):
+        count = count+1
+        cv2.circle(output, (circles[i][0], circles[i]
+                            [1]), circles[i][2], (0, 255, 0), 2)
 
-    # img2 = Image.fromarray(output.astype("uint8"))
-    # rawBytes = io.BytesIO()
-    # img2.save(rawBytes, "JPEG")
-    # rawBytes.seek(0)
-    # img_64 = base64.b64encode(rawBytes.read()).decode('utf-8')
-    # request_url = request.remote_addr
-    # return {"image64": img_64, "points": count}
+    img2 = Image.fromarray(output.astype("uint8"))
+    rawBytes = io.BytesIO()
+    img2.save(rawBytes, "JPEG")
+    rawBytes.seek(0)
+    img_64 = base64.b64encode(rawBytes.read()).decode('utf-8')
+    request_url = request.remote_addr
+    return {"image64": img_64, "points": count}
 
 
 # if __name__ == '__main__':
